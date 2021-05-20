95120f91a19296c9e31457e0072e3c89de03efa6
Urbi Ghosh <55971386+UrbiGhosh@users.noreply.github.com>
2021-05-20 12:54:17 +0530

    Create vulnerable.js

---
+++ Assignment4/Answer/vulnerable.js
@@ -0,0 +1,59 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'Password123!',
+	database : 'cs253_asgn4_data'
+});
+
+var app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+        var q = 'SELECT * FROM user_data WHERE email = \''+username + '\' AND password = \'' +password+'\'';
+		var temp=connection.query(q, [], function(error, results, fields) {
+			
+            if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Hacker caught red-handed!');
+			}			
+			response.end();
+		});
+        console.log(temp.sql);
+	} else {
+		response.send('Enter login data');
+		response.end();
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Success!');
+	} else {
+		response.send('Try logging in!');
+	}
+	response.end();
+});
+
+app.listen(3000);
